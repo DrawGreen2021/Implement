2104c9ea5550a9c3b85a131d457d6bcde811d262
DESKTOP-9HJV8O4\GIGABYTE <ssin31400@gmail.com>
2021-09-30 13:27:04 +0900

    이메일 input 태그 수정에 따라 파라미터 받는 방식 수정, dao를 싱글톤 패턴으로 설정 후 인스턴스 받아오기

--- src/com/drawgreen/corpcollector/command/JoinCommand.java
+++ src/com/drawgreen/corpcollector/command/JoinCommand.java
@@ -1,8 +1,5 @@
 package com.drawgreen.corpcollector.command;
 
-import java.sql.Date;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,16 +10,15 @@ public class JoinCommand implements Command{
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response){
 		// TODO Auto-generated method stub
-		//SimpleDateFormat transFormat = new SimpleDateFormat("yyyymmdd");
 		
 		String id = request.getParameter("id");
 		String pw = request.getParameter("pw");
 		String name = request.getParameter("name");
-		String email = request.getParameter("email1") + "@" + request.getParameterValues("email2");
+		String email = request.getParameter("email");
 		String birth = request.getParameter("year")+"-"+request.getParameter("month")+"-"+request.getParameter("day");
 		String gender = request.getParameter("gender");
 		
-		MemberDAO memberDAO = new MemberDAO();
-		memberDAO.insertMember(id, pw, name, email, birth, gender);
+		MemberDAO dao = MemberDAO.getInstance();
+		dao.insertMember(id, pw, name, email, birth, gender);
 	}
 }
