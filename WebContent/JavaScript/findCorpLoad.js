01b54f8f9137e77f61e074673120c10dc0961816
DESKTOP-9HJV8O4\GIGABYTE <ssin31400@gmail.com>
2021-10-09 19:52:43 +0900

    전체 목록보기 버튼을 클릭했을 때 초기 리스트를 불러오도록 액션 실행

--- WebContent/JavaScript/findCorpLoad.js
+++ WebContent/JavaScript/findCorpLoad.js
@@ -8,8 +8,6 @@ document.getElementsByTagName('head')[0].appendChild(script);
 
 var doubleSubmitFlag = false;
 
-document.getElementById('keyword').value = saveValue('keyword');
-
 
 function fncSubmit(){
     if(doubleSubmitFlag){
@@ -20,30 +18,12 @@ function fncSubmit(){
     }
 }
 
-window.onload=function(){
-	function getParameterByName(name) { 
-		name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]"); 
-		var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"), 
-			results = regex.exec(location.search); 
-		return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " ")); 
-	}
-
-	var keyword = getParameterByName('keyword');
-	$('#keyword').val(keyword);
-	console.log(keyword);
+function resetKeyword() {
+	var form = document.findGreenCorp;
+	var noKeyword = "";
 	
-	
-}
-
-function saveValue(e){
-    var id = e.id;  // get the sender's id to save it . 
-    var val = e.value; // get the value. 
-    localStorage.setItem(id, val);// Every time user writing something, the localStorage's value will override . 
-}
-
-function getSavedValue(v){
-    if (!localStorage.getItem(v)) {
-        return "";// You can change this to your defualt value. 
-    }
-    return localStorage.getItem(v);
+	form.keyword.value = noKeyword;
+	form.actiond = "FindGreenCorp.do";
+	form.method = "get";
+	form.submit();
 }
