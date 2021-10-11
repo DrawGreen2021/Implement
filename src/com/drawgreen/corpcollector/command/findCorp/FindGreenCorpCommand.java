56abf49034c831ea301adad577228544ab458be5
DESKTOP-9HJV8O4\GIGABYTE <ssin31400@gmail.com>
2021-10-11 10:49:58 +0900

    Command에서 리스트와 행 개수 받아오는 조건문 수정

--- src/com/drawgreen/corpcollector/command/findCorp/FindGreenCorpCommand.java
+++ src/com/drawgreen/corpcollector/command/findCorp/FindGreenCorpCommand.java
@@ -12,12 +12,7 @@ import com.drawgreen.corpcollector.dto.GreenCorpDTO;
 import com.drawgreen.corpcollector.util.Pager;
 
 public class FindGreenCorpCommand implements Command{
-	ServletContext context;
 	Pager pager = new Pager();
-	
-	public FindGreenCorpCommand(ServletContext context) {
-		this.context = context;
-	}
 	
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) {
@@ -25,27 +20,17 @@ public class FindGreenCorpCommand implements Command{
 		String keyword = request.getParameter("keyword")==null?"":request.getParameter("keyword");
 		String page_str = request.getParameter("page");
 		int page = Integer.parseInt(page_str);
-		String corpType = (String) context.getAttribute("greenCorp");
-		
 		
 		GreenCorpDAO dao = GreenCorpDAO.getInstance();
 		ArrayList<GreenCorpDTO> corpList = null;
 		int rowCount = 0;
 		
-		// 초기 리스트 불러오기
-		if(!dao.isAllSearched()) {
+		// 키워드 없을 때
+		if(keyword.equals("")) {
 			corpList = dao.getCorpList(page);
-			rowCount = dao.getRowCount(corpType);
-			context.setAttribute("allGreenRows", rowCount);
-
-			dao.setAllSearched(true);
+			rowCount = dao.getAllRowCount();
 		}
-		// 키워드 없이 검색 버튼을 눌렀을 때
-		else if(keyword.equals("")) {
-			corpList = dao.getCorpList(page);
-			rowCount = (int) context.getAttribute("allGreenRows");
-		}
-		// 키워드가 있는 경우
+		// 키워드가 있을 때
 		else {
 			corpList = dao.getCorpList(keyword, page);
 			rowCount = dao.getRowCount_byKeyword();
