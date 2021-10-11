097893f82d1b8f1077bbb92580f39e97a898ad56
DESKTOP-9HJV8O4\GIGABYTE <ssin31400@gmail.com>
2021-10-11 10:58:41 +0900

    가족 친화 인증 기업 관련 코드 작성

---
+++ src/com/drawgreen/corpcollector/dao/FamilyFriendlyCorpDAO.java
@@ -0,0 +1,187 @@
+package com.drawgreen.corpcollector.dao;
+
+import java.sql.DriverManager;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+import com.drawgreen.corpcollector.dto.FamilyFriendlyCorpDTO;
+
+public class FamilyFriendlyCorpDAO extends CorpDAO {
+	private int allRowCount;
+	private int pageRowCount;
+	// 키워드 검색 결과에 해당하는 연번을 저장할 리스트
+	private ArrayList<Integer> serialNums;
+	// 키워드 값을 저장할 변수
+	private String beforeKeyword;
+
+	private FamilyFriendlyCorpDAO() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			pageRowCount = 10;
+			allRowCount = getRowCount("가족친화인증기업");
+			serialNums = new ArrayList<Integer>();
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+
+	private static class InnerInstance_CorpDAO {
+		private static final FamilyFriendlyCorpDAO familyFriendlyCorpDAO = new FamilyFriendlyCorpDAO();
+	}
+
+	public static FamilyFriendlyCorpDAO getInstance() {
+		return InnerInstance_CorpDAO.familyFriendlyCorpDAO;
+	}
+
+	public int getAllRowCount() {
+		return allRowCount;
+	}
+
+	public void setAllRowCount(int allRowCount) {
+		this.allRowCount = allRowCount;
+	}
+
+	// 검색 키워드가 없다면 연번으로 행 개수만큼 불러오기
+	public ArrayList<FamilyFriendlyCorpDTO> getCorpList(int page) {
+		ArrayList<FamilyFriendlyCorpDTO> familyFriendlyCorpDTOs = new ArrayList<FamilyFriendlyCorpDTO>();
+		String query = "SELECT * FROM 가족친화인증기업 WHERE 연번 BETWEEN ? AND ?";
+
+		try {
+			connection = DriverManager.getConnection(url, userId, userPw);
+			preparedStatement = connection.prepareStatement(query);
+			preparedStatement.setInt(1, 1 + (page * pageRowCount - pageRowCount));
+			preparedStatement.setInt(2, page * pageRowCount);
+
+			resultSet = preparedStatement.executeQuery();
+
+			while (resultSet.next()) {
+				int serial_number = resultSet.getInt("연번");
+				String company_name = resultSet.getString("기업명");
+				String division = resultSet.getString("분류");
+				String city_state = resultSet.getString("시도");
+
+				FamilyFriendlyCorpDTO dto = new FamilyFriendlyCorpDTO(serial_number, company_name, division, city_state);
+				familyFriendlyCorpDTOs.add(dto);
+				//System.out.println(dto.getCity_sate());
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		} finally {
+			try {
+				if (connection != null)
+					connection.close();
+				if (preparedStatement != null)
+					preparedStatement.close();
+				if (resultSet != null)
+					resultSet.close();
+			} catch (Exception e2) {
+				// TODO: handle exception
+				e2.printStackTrace();
+			}
+		}
+
+		return familyFriendlyCorpDTOs;
+	}
+
+	// 검색어가 있을 경우 기업 리스트 받아오기
+	public ArrayList<FamilyFriendlyCorpDTO> getCorpList(String keyword, int page) {
+		// TODO Auto-generated method stub
+		ArrayList<FamilyFriendlyCorpDTO> familyFriendlyCorpDTOs = new ArrayList<FamilyFriendlyCorpDTO>();
+
+		// 검색어가 달라졌으면 해당 연번 다시 select
+		if (beforeKeyword == null) {
+			beforeKeyword = keyword;
+			serialNums = getSerialNumQuery(keyword, serialNums);
+		} else if (!beforeKeyword.equals(keyword)) {
+			serialNums.clear();
+			serialNums = getSerialNumQuery(keyword, serialNums);
+		}
+
+		String getCorpListQuery = "SELECT * FROM 가족친화인증기업 " + "WHERE 연번 IN(";
+		// 0~9, 10~19 ...
+		int startNum = page * pageRowCount - pageRowCount;
+		int lastNum = page * pageRowCount;
+
+		StringBuilder builder = new StringBuilder(getCorpListQuery);
+
+		for (int i = startNum; i < lastNum && i < serialNums.size(); i++) {
+			builder.append(serialNums.get(i));
+
+			if (i < lastNum - 1)
+				builder.append(",");
+		}
+		builder.append(")");
+		getCorpListQuery = builder.toString();
+
+		try {
+			connection = DriverManager.getConnection(url, userId, userPw);
+			preparedStatement = connection.prepareStatement(getCorpListQuery);
+			resultSet = preparedStatement.executeQuery();
+
+			while (resultSet.next()) {
+				int serial_number = resultSet.getInt("연번");
+				String company_name = resultSet.getString("기업명");
+				String division = resultSet.getString("분류");
+				String city_state = resultSet.getString("시도");
+
+				FamilyFriendlyCorpDTO dto = new FamilyFriendlyCorpDTO(serial_number, company_name, division, city_state);
+				familyFriendlyCorpDTOs.add(dto);
+			}
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			familyFriendlyCorpDTOs = null;
+		} finally {
+			try {
+				if (connection != null)
+					connection.close();
+				if (preparedStatement != null)
+					preparedStatement.close();
+				if (resultSet != null)
+					resultSet.close();
+			} catch (Exception e2) {
+				// TODO: handle exception
+				e2.printStackTrace();
+			}
+		}
+
+		return familyFriendlyCorpDTOs;
+	}
+
+	// 검색 키워드가 존재하는 행의 연번 알아오기
+	public ArrayList<Integer> getSerialNumQuery(String keyword, ArrayList<Integer> serialNums) {
+
+		// 키워드 공백으로 분리
+		StringTokenizer tokenizer = new StringTokenizer(keyword);
+
+		String query = "SELECT 연번 FROM familyFriendlyCorp_view " + "WHERE text REGEXP('";
+		StringBuffer buffer = new StringBuffer(query);
+		buffer.append(tokenizer.nextToken() + "'");
+		while (tokenizer.hasMoreTokens()) {
+			buffer.append("|'");
+			buffer.append(tokenizer.nextToken() + "'");
+		}
+		buffer.append(")");
+		query = buffer.toString();
+
+		try {
+			connection = DriverManager.getConnection(url, userId, userPw);
+			preparedStatement = connection.prepareStatement(query);
+			resultSet = preparedStatement.executeQuery();
+
+			while (resultSet.next())
+				serialNums.add(resultSet.getInt("연번"));
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+		return serialNums;
+	}
+
+	public int getRowCount_byKeyword() {
+		return serialNums.size();
+	}
+}
---
+++ src/com/drawgreen/corpcollector/dto/FamilyFriendlyCorpDTO.java
@@ -0,0 +1,42 @@
+package com.drawgreen.corpcollector.dto;
+
+public class FamilyFriendlyCorpDTO {
+	private int serial_number;
+	private String company_name;
+	private String division;
+	private String city_state;
+	
+	public FamilyFriendlyCorpDTO(int serial_number, String company_name, String division, String city_sate) {
+		super();
+		this.serial_number = serial_number;
+		this.company_name = company_name;
+		this.division = division;
+		this.city_state = city_sate;
+	}
+	public int getSerial_number() {
+		return serial_number;
+	}
+	public void setSerial_number(int serial_number) {
+		this.serial_number = serial_number;
+	}
+	public String getCompany_name() {
+		return company_name;
+	}
+	public void setCompany_name(String company_name) {
+		this.company_name = company_name;
+	}
+	public String getDivision() {
+		return division;
+	}
+	public void setDivision(String division) {
+		this.division = division;
+	}
+	public String getCity_sate() {
+		return city_state;
+	}
+	public void setCity_sate(String city_sate) {
+		this.city_state = city_sate;
+	}
+	
+	
+}
