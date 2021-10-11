ce7fcc4a7a192b24ec1c017d302d455758761e25
DESKTOP-9HJV8O4\GIGABYTE <ssin31400@gmail.com>
2021-10-11 10:53:44 +0900

    안 쓰는 함수 CorpDAO에서 삭제

--- src/com/drawgreen/corpcollector/dao/CorpDAO.java
+++ src/com/drawgreen/corpcollector/dao/CorpDAO.java
@@ -13,37 +13,6 @@ public abstract class CorpDAO {
 	protected String userPw = "general_user_password"; 
 	protected String url = "jdbc:mysql://corpcollector.ciqetekukvwo.ap-northeast-2.rds.amazonaws.com:3306/Corp";
 	
-	public int getRowCount(String corpType, String field, String keyword) {
-		int rowCount = 0;
-		String query = "SELECT count(*) FROM ("
-				+ "SELECT * FROM "+corpType+" WHERE "+field+" LIKE ?)getRowCount";
-		
-		try {
-			connection = DriverManager.getConnection(url, userId, userPw);
-			preparedStatement = connection.prepareStatement(query);
-			preparedStatement.setString(1, "%"+keyword+"%");
-			resultSet = preparedStatement.executeQuery();
-			
-			resultSet.next();
-			rowCount = resultSet.getInt(1);
-			
-		} catch (Exception e) {
-			// TODO: handle exception
-			e.printStackTrace();
-		} finally {
-			try {
-				if(connection!=null) connection.close();
-				if(preparedStatement!=null) preparedStatement.close();
-				if(resultSet!=null) resultSet.close();
-			} catch (Exception e2) {
-				// TODO: handle exception
-				e2.printStackTrace();
-			}
-		}
-		
-		return rowCount;
-	}
-	
 	public int getRowCount(String corpType) {
 
 		int rowCount = 0;
