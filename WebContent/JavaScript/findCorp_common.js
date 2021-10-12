8f89841307bd5870996369c85da99b471d85adb4
DESKTOP-9HJV8O4\GIGABYTE <ssin31400@gmail.com>
2021-10-12 22:08:44 +0900

    관심기업 버튼을 클릭하면 AJAX을 통해 AddFavoriteCorp.do가 실행되도록 작성

--- WebContent/JavaScript/findCorp_common.js
+++ WebContent/JavaScript/findCorp_common.js
@@ -1,6 +1,11 @@
 /**
  * 
  */
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
 
 // 기업 찾기 각 페이지마다 적용되는 리셋버튼 함수
 function resetKeyword() {
@@ -11,4 +16,32 @@ function resetKeyword() {
 	form.action = "FindCorp.do";
 	form.method = "get";
 	form.submit();
+}
+
+// 관심 기업 등록
+function addFavoriteCorp(button) {
+	
+	var serial_number = button.value;
+	var corpType = $('#corpType').val();
+	var corpName = $('#corpName'+serial_number).text();
+	
+	$.ajax({
+        type:'post',
+        async:false,
+        url:'AddFavoriteCorp.do',
+        dataType:'text',
+        data:{"serial_number":serial_number, "corpType":corpType, "corpName":corpName},
+        success: function(data, textStatus) {
+            if(data === 'not-login') {
+                alert("로그인해주세요.");     
+            } else if(data === 'addFavoriteCorp'){
+                $(button).text('★');
+            } else {
+            	$(button).text('☆');
+            }
+        },
+        error:function (data, textStatus) {
+            console.log('error');
+        }
+    });   //ajax
 }
