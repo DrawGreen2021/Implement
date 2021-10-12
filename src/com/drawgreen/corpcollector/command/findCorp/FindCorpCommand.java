19224a92c1ba588fc4de399bbcb16ac05438a3ee
DESKTOP-9HJV8O4\GIGABYTE <ssin31400@gmail.com>
2021-10-12 10:25:02 +0900

    사회적 기업 찾기 기능 구현, FindCorpCommand에 corpType에 따른 조건(socialCorp) 추가

--- src/com/drawgreen/corpcollector/command/findCorp/FindCorpCommand.java
+++ src/com/drawgreen/corpcollector/command/findCorp/FindCorpCommand.java
@@ -9,6 +9,7 @@ import com.drawgreen.corpcollector.command.Command;
 import com.drawgreen.corpcollector.dao.CorpDAO;
 import com.drawgreen.corpcollector.dao.FamilyFriendlyCorpDAO;
 import com.drawgreen.corpcollector.dao.GreenCorpDAO;
+import com.drawgreen.corpcollector.dao.SocialCorpDAO;
 import com.drawgreen.corpcollector.dao.TalentDevelopmentCorpDAO;
 import com.drawgreen.corpcollector.dao.YouthFriendlyCorpDAO;
 import com.drawgreen.corpcollector.dto.CorpDTO;
@@ -33,7 +34,9 @@ public class FindCorpCommand implements Command {
 			dao = GreenCorpDAO.getInstance();
 		} else if (corpType.equals("talentDevelopmentCorp")) {
 			dao = TalentDevelopmentCorpDAO.getInstance();
-		} else if (corpType.equals("familyFriendlyCorp")) {
+		} else if (corpType.equals("socialCorp")) {
+			dao = SocialCorpDAO.getInstance();
+		}else if (corpType.equals("familyFriendlyCorp")) {
 			dao = FamilyFriendlyCorpDAO.getInstance();
 		} else if (corpType.equals("youthFriendlyCorp")) {
 			dao = YouthFriendlyCorpDAO.getInstance();
