cac4e72686fda32b299026de1dccc5857c468c4a
DESKTOP-9HJV8O4\GIGABYTE <ssin31400@gmail.com>
2021-10-19 23:50:32 +0900

    기업 찾기 페이지에서 관심기업을 표시할 함수 추가 - 키워드가 없을 경우 관심기업 아이디 불러오기 - 키워드가 있을 경우 관심기업 아이디 불러오기

--- src/com/drawgreen/corpcollector/dao/FavoriteCorpDAO.java
+++ src/com/drawgreen/corpcollector/dao/FavoriteCorpDAO.java
@@ -4,6 +4,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 
 public class FavoriteCorpDAO {
 	private Connection connection = null;
@@ -15,10 +18,12 @@ public class FavoriteCorpDAO {
 	private String rootId = "drawgreen";
 	private String rootPw = "drawgreen2021"; 
 	private String url = "jdbc:mysql://corpcollector.ciqetekukvwo.ap-northeast-2.rds.amazonaws.com:3306/Member";
+	private int pageRowCount;
 	
 	private FavoriteCorpDAO() {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
+			pageRowCount = 10;
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
@@ -120,6 +125,102 @@ public class FavoriteCorpDAO {
 				e2.printStackTrace();
 			}
 		}
+	}
+	
+	// 키워드가 없을 경우 관심기업 아이디 불러오기
+	public int[] getFavoirteSerialNums(int page, String corpType, String user_id, int allRowCount) {
+		int[] favoriteSerialNums = new int[pageRowCount];
+		
+		// 관심 기업에서 유저 아이디가 현재 접속 중인 아이디이고, 기업 아이디가 페이지에 표시된 연번 사이에 있는 리스트
+		String query = "SELECT "+corpType+"_id FROM 관심기업 "
+				+ "WHERE user_id = ? AND "+corpType+"_id BETWEEN ? AND ? "
+						+ "ORDER BY "+corpType+"_id ASC";
+		
+		try {
+			connection = DriverManager.getConnection(url, userId, userPw);
+			preparedStatement = connection.prepareStatement(query);
+			preparedStatement.setString(1, user_id);
+			preparedStatement.setInt(2, 1 + (page * pageRowCount - pageRowCount));
+			preparedStatement.setInt(3, page*pageRowCount < allRowCount?
+					page*pageRowCount:allRowCount);
+			
+			resultSet = preparedStatement.executeQuery();
+			
+			// 관심 기업 아이디를 배열에 저장
+			while(resultSet.next()) {
+				int corp_id = resultSet.getInt(corpType+"_id");
+				favoriteSerialNums[corp_id % pageRowCount - 1] = corp_id;
+			}
+		
+		} catch (Exception e) {
+			// TODO: handle exception
+			favoriteSerialNums = null;
+		} finally {
+			try {
+				if (connection != null)
+					connection.close();
+				if (preparedStatement != null)
+					preparedStatement.close();
+				if (resultSet != null)
+					resultSet.close();
+			} catch (Exception e2) {
+				// TODO: handle exception
+				e2.printStackTrace();
+			}
+		}
+		
+		return favoriteSerialNums;
+	}
+	
+	// 키워드가 있을 경우 관심기업 아이디 불러오기
+	public int[] getFavoirteSerialNums(int page, String corpType, String user_id, ArrayList<Integer> serialNums) {
+		int[] favoriteSerialNums = new int[pageRowCount];
+		
+		// 관심 기업에서 유저 아이디가 현재 접속 중인 아이디이고, 기업 아이디가 페이지에 표시된 연번 사이에 있는 리스트
+		String query = "SELECT "+corpType+"_id FROM 관심기업 "
+				+ "WHERE user_id = ? AND "+corpType+"_id IN(";
+		int startNum = page * pageRowCount - pageRowCount;
+		int lastNum = page * pageRowCount;
+		
+		StringBuilder builder = new StringBuilder(query);
+
+		builder.append(serialNums.get(startNum));
+		for (int i = startNum+1; i < lastNum && i < serialNums.size(); i++) {
+			builder.append(",");
+			builder.append(serialNums.get(i));	
+		}
+		builder.append(") ORDER BY "+corpType+"_id ASC");
+		query = builder.toString();
+		
+		try {
+			connection = DriverManager.getConnection(url, userId, userPw);
+			preparedStatement = connection.prepareStatement(query);
+			preparedStatement.setString(1, user_id);
+			resultSet = preparedStatement.executeQuery();
+			
+			// serialNums 인덱스에 맞춰서 배열 값 저장
+			while(resultSet.next()) {
+				int corp_id = resultSet.getInt(corpType+"_id"); 
+				favoriteSerialNums[serialNums.indexOf(corp_id) % pageRowCount] = corp_id;
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			favoriteSerialNums = null;
+		} finally {
+			try {
+				if (connection != null)
+					connection.close();
+				if (preparedStatement != null)
+					preparedStatement.close();
+				if (resultSet != null)
+					resultSet.close();
+			} catch (Exception e2) {
+				// TODO: handle exception
+				e2.printStackTrace();
+			}
+		}
+		
+		return favoriteSerialNums;
 	}
 	
 	// 기업 테이블명 가져오기
