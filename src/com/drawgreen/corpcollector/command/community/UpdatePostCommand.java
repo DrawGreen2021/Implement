ef860903efca3bd1a7c6840c9df4917dea3bfa00
DESKTOP-9HJV8O4\GIGABYTE <ssin31400@gmail.com>
2021-10-26 08:16:13 +0900

    게시글을 수정/작성하는 커맨드

---
+++ src/com/drawgreen/corpcollector/command/community/UpdatePostCommand.java
@@ -0,0 +1,53 @@
+package com.drawgreen.corpcollector.command.community;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.drawgreen.corpcollector.command.Command;
+import com.drawgreen.corpcollector.dao.NoticePostDAO;
+import com.drawgreen.corpcollector.dao.PostDAO;
+
+public class UpdatePostCommand implements Command{
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		String title = request.getParameter("title");
+		String private_Writing = request.getParameter("private_Writing");
+		String private_Writer = request.getParameter("private_Writer");
+		String content = request.getParameter("content");
+		String boardName = request.getParameter("boardName");
+		String board_number_str = request.getParameter("board_number");
+		int board_number = Integer.parseInt(board_number_str);
+		
+		String nextPage = "PostView.do?board_number="+board_number+"&boardName="+boardName;
+		PostDAO dao = null;
+		if (boardName.equals("공지사항")) {
+			dao = NoticePostDAO.getInstance();
+		}
+		else {
+			
+		}
+		
+		boolean updateOk = dao.updatePost(board_number, title, private_Writing, private_Writer, content);
+		
+		if (updateOk) {
+			PrintWriter out;
+			try {
+				out = response.getWriter();
+				out.println("<script>");
+				out.println("alert('게시글이 수정되었습니다.'); ");
+				out.println("location.href='"+nextPage+"';");
+				out.println("</script>");
+				out.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
---
+++ src/com/drawgreen/corpcollector/command/community/WritePostCommand.java
@@ -0,0 +1,54 @@
+package com.drawgreen.corpcollector.command.community;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.drawgreen.corpcollector.command.Command;
+import com.drawgreen.corpcollector.dao.NoticePostDAO;
+import com.drawgreen.corpcollector.dao.PostDAO;
+
+public class WritePostCommand implements Command{
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		String title = request.getParameter("title");
+		String private_Writing = request.getParameter("private_Writing");
+		String writer = request.getParameter("writer");
+		String writer_id = request.getParameter("writer_id");
+		String private_Writer = request.getParameter("private_Writer");
+		String content = request.getParameter("content");
+		String boardName = request.getParameter("boardName");
+		
+		String nextPage = "";
+		PostDAO dao = null;
+		if (boardName.equals("공지사항")) {
+			nextPage = "notice.jsp";
+			dao = NoticePostDAO.getInstance();
+		}
+		else {
+			nextPage = "feedback.jsp";
+		}
+		
+		boolean writeOk = dao.writePost(title, private_Writing, writer, writer_id, private_Writer, content);
+		
+		if (writeOk) {
+			PrintWriter out;
+			try {
+				out = response.getWriter();
+				out.println("<script>");
+				out.println("alert('게시글이 등록되었습니다.'); ");
+				out.println("location.href='"+nextPage+"';");
+				out.println("</script>");
+				out.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
