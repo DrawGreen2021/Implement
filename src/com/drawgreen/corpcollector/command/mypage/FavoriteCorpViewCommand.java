7c6b79be844e5439c1abd35db63e731ad9578a05
DESKTOP-9HJV8O4\GIGABYTE <ssin31400@gmail.com>
2021-10-27 21:56:31 +0900

    관심기업에 표시할 관심기업 연번 및 업체명 얻어오는 메소드, 사용자가 등록한 관심기업 개수 가져오는 메소드, 관심기업 테이블에서 기업정보 삭제하는 메소드 및 커맨드 작성

---
+++ src/com/drawgreen/corpcollector/command/mypage/FavoriteCorpViewCommand.java
@@ -0,0 +1,33 @@
+package com.drawgreen.corpcollector.command.mypage;
+
+import java.util.HashMap;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.drawgreen.corpcollector.command.Command;
+import com.drawgreen.corpcollector.dao.FavoriteCorpDAO;
+import com.drawgreen.corpcollector.dto.MemberDTO;
+import com.drawgreen.corpcollector.util.Pager;
+
+public class FavoriteCorpViewCommand implements Command{
+	Pager pager = new Pager();
+	
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		HttpSession httpSession = request.getSession();
+		MemberDTO user = (MemberDTO) httpSession.getAttribute("MemberDTO");
+		String page_str = request.getParameter("page");
+		int page = Integer.parseInt(page_str);
+		
+		FavoriteCorpDAO dao = FavoriteCorpDAO.getInstance();
+		HashMap<String, HashMap<Integer, String>> favCorpMap = dao.getFavoriteSerialNums_forMyPage(user.getId(), page);
+		int rowCount = dao.getRowCount(user.getId(), page);
+		
+		request.setAttribute("favCorpMap", favCorpMap);
+		pager.setNumbers(page, rowCount, request, response);
+	}
+
+}
